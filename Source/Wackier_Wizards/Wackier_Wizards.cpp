e22b41bcd01194e1eb295100908215c1d7e532dc
Ben Higham <94679239+DidEARobya@users.noreply.github.com>
2025-02-02 04:29:24 +0000

    Fixed C++ Issue and Renamed to "Wackier_Wizards"

---
+++ Source/Wackier_Wizards/Wackier_Wizards.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Wackier_Wizards.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Wackier_Wizards, "Wackier_Wizards" );
---
+++ Source/Wackier_WizardsEditor.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Wackier_WizardsEditorTarget : TargetRules
+{
+    public Wackier_WizardsEditorTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+
+        ExtraModuleNames.AddRange(new string[] { "Wackier_Wizards" });
+    }
+}
