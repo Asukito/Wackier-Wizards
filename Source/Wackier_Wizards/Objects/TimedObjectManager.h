70cb224a0d6a837b89793928e402a2395e5944ea
Ben Higham <94679239+DidEARobya@users.noreply.github.com>
2025-02-04 12:08:38 +0000

    TimedPillars First Iteration

---
+++ Source/Wackier_Wizards/Objects/TimedObjectManager.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TimedObjectManager.generated.h"
+
+class ATimedPillar;
+
+UCLASS()
+class WACKIER_WIZARDS_API ATimedObjectManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATimedObjectManager();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void Init();
+	void NextGroup();
+
+private:
+	UPROPERTY(EditAnywhere)
+	TMap<TObjectPtr<ATimedPillar>, int> _pillars;
+	UPROPERTY(EditAnywhere)
+	float _delayBetweenSwitch;
+
+	int _index;
+	float _timer;
+};
