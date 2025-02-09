89eab7113164df6e92f3686dacbbc0542c6658a0
stalker-doge <94372701+stalker-doge@users.noreply.github.com>
2025-02-09 20:14:35 +0000

    VR stuff

--- Source/Wackier_Wizards/Characters/Player/VRCharacter.cpp
+++ Source/Wackier_Wizards/Characters/Player/VRCharacter.cpp
@@ -2,12 +2,19 @@
 
 
 #include "VRCharacter.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedPlayerInput.h"
+
 
 // Sets default values
 AVRCharacter::AVRCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+
 
 }
 
@@ -29,6 +36,54 @@ void AVRCharacter::Tick(float DeltaTime)
 void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	// Get the player controller
+	APlayerController* PC = Cast<APlayerController>(GetController());
+
+	//Get the local player subsystem
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer());
+
+	//Clear out the existing mappings, and add our mapping
+	Subsystem->ClearAllMappings();
+	Subsystem->AddMappingContext(InputMappingContext, 0);
+
+	//Get the Enhanced Input Component
+	UEnhancedInputComponent* PEI = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	// Bind the actions
+	PEI->BindAction(inputMove, ETriggerEvent::Triggered, this, &AVRCharacter::MovePlayer);
+	PEI->BindAction(inputShootSpell, ETriggerEvent::Triggered, this, &AVRCharacter::ShootSpell);
+	PEI->BindAction(inputChangeSpell, ETriggerEvent::Triggered, this, &AVRCharacter::ChangeSpell);
+}
+
+void AVRCharacter::ShootSpell()
+{
+	//log that the player is shooting a spell
+	UE_LOG(LogTemp, Warning, TEXT("Player is shooting a spell"));
+}
+
+void AVRCharacter::ChangeSpell()
+{//log that the player is changing the spell
+	UE_LOG(LogTemp, Warning, TEXT("Player is changing the spell"));
+}
+
+void AVRCharacter::MovePlayer(const FInputActionValue& Value)
+{
+	//log the value of the input action value
+	
+	UE_LOG(LogTemp, Warning, TEXT("Value: %s"), *Value.ToString());
+	//checks if the player controller is valid
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		//gets the forward vector of the player controller
+		FVector ForwardVector = PC->GetControlRotation().Vector();
+		//gets the right vector of the player controller
+		FVector RightVector = ForwardVector.RotateAngleAxis(90, FVector(0, 0, 1));
+		//creates a movement vector
+		FVector MovementVector = ForwardVector * Value.Get<FVector>().Y + RightVector * Value.Get<FVector>().X;
+		//moves the player character
+		AddMovementInput(MovementVector, 1);
+		//prints the movement vector
+		UE_LOG(LogTemp, Warning, TEXT("Movement Vector: %s"), *MovementVector.ToString());
+	}
 
 }
 
--- Source/Wackier_Wizards/Characters/Player/VRCharacter.h
+++ Source/Wackier_Wizards/Characters/Player/VRCharacter.h
@@ -7,6 +7,7 @@
 #include "InputMappingContext.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "EnhancedPlayerInput.h"
 #include "VRCharacter.generated.h"
 
 
@@ -19,6 +20,21 @@ public:
 	// Sets default values for this character's properties
 	AVRCharacter();
 
+	// Input mapping context for VRCharacter
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input Mapping")
+	class UInputMappingContext* InputMappingContext;
+
+	// Enhanced input subsystem for VRCharacter
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input Mapping")
+	class UInputAction* inputMove;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input Mapping")
+	class UInputAction* inputShootSpell;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input Mapping")
+	class UInputAction* inputChangeSpell;
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -29,5 +45,17 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Function to shoot a spell
+	UFUNCTION(BlueprintCallable, Category = "Player Spell")
+	void ShootSpell();
+
+	// Function to change the spell
+	UFUNCTION(BlueprintCallable, Category = "Player Spell")
+	void ChangeSpell();
+
+	// Function to move the player character 
+	UFUNCTION(BlueprintCallable, Category = "Player Movement")
+	void MovePlayer(const FInputActionValue& Value);
 
 };
